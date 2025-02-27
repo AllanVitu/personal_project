f62a8645d139ec8a486fc33387bc8ba951432b36
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-02-27 14:40:57 +0100

    Création du bouton s'inscrire

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/styles.css">
+    <link rel="stylesheet" href="./docs/assets/style2.css">
     <title>S.P.R.E.D.</title>
 </head>
 <body>
@@ -11,33 +11,33 @@
         
     //image de fond du site.
 <div>
-    <img src="/imgs/matrix_2_button.jpg" alt="matrix_img">
+    <img src="./docs/imgs/matrix_2_button.jpg" alt="matrix_img">
 </div>
             
 
     //Commencement du PHP.
 
     <?php 
-    include 'includes/_nav.php';
+    include './docs/includes/_nav.php'
     ?>
     </header>
     <main>
 
     <?php
-        include 'includes/_java.php';
+        include './docs/includes/_java.php';
     ?>
 
         <?php
             if(isset($_GET['action'])) {
-                include 'pages/' . $_GET('action') . '.php';
+                include './pages/' . $_GET('action') . '.php';
             }
             else {
-                include 'pages/accueil.php';
+                include './pages/accueil.php';
             }  
         ?>
     </main>
     <?php 
-    include 'includes/_footer.php';
+    include './docs/includes/_footer.php';
     ?>        
     </body>
 </html>
