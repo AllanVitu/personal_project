3aabd21fbed439f834f80a201e0939e0d2da46f2
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-05-05 08:13:03 +0200

    Push origin

---
+++ docs/README.md
@@ -0,0 +1,10 @@
+1. Creer la structure HTML [SPRED]
+2. Creer le CSS, JS et ou PHP [SPRED]
+3. Creer la structure HTML et CSS du formulaire
+4. Faire la BDD
+
+
+
+
+
+
--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,1 +1,30 @@
-const btnSinscrire = document.querySelector("#buttonSingIn")
+// script.js
+const container = document.getElementById('container');
+const loginButton = document.getElementById('login');
+const signUpButton = document.getElementById('signUp');
+const btnNoCompte = document.getElementById('btnNoCompte');
+const btnOkCompte =  document.getElementById('btnOkCompte');
+
+// Ajoute la classe "active" pour afficher le formulaire d'inscription
+signUpButton.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+// Supprime la classe "active" pour afficher le formulaire de connexion
+loginButton.addEventListener('click', () => {
+    container.classList.remove('active');
+});
+
+btnNoCompte.addEventListener('click', () =>{
+    let toto = document.querySelector('.container');
+
+    toto.classList.toggle('active');
+
+})
+
+btnOkCompte.addEventListener('click', () =>{
+    let toto = document.querySelector('.container');
+
+    toto.classList.toggle('active');
+
+})
